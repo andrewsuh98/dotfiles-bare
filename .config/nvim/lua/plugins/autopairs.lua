3874d64b0756cae1d18d2aee14a4f7d3d35ac039
andrewsuh98 <andrewsuh98@gmail.com>
2024-12-06 22:20:54 -0500

    reset lazyvim

--- .config/nvim/lua/plugins/autopairs.lua
+++
@@ -1,7 +0,0 @@
--- This plugin used to trigger insertion of "(" after autocomplete on method/function.
-return {
-	"windwp/nvim-autopairs",
-	opts = {
-		disable_filetype = { "TelescopePrompt", "vim" },
-	},
-}
