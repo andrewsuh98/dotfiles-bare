fff21e4284e950a78c43aa26bbcc430d9a6a7d7a
andrewsuh98 <andrewsuh98@gmail.com>
2025-02-21 14:36:05 -0500

    remove telescope.nvim -> using fzf-lua instead

---
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,12 @@
+return {
+	"ibhagwan/fzf-lua",
+	opts = {
+		defaults = {
+			-- NOTE: uncomment to disable all interactive headers
+			-- no_header_i = true,
+		},
+		files = {
+			header = false,
+		},
+	},
+}
