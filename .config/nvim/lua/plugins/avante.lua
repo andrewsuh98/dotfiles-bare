5d962f8e6055969b0f344f137214f4a64fdfa3a0
Andrew Suh <asuh1@wayfair.com>
2025-06-16 10:01:16 -0400

    disable avante.nvim

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,26 +1,26 @@
 return {
-	"yetone/avante.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- add any opts here
-		-- for example
-		provider = "claude",
-		claude = {
-			endpoint = "https://api.anthropic.com",
-			model = "claude-3-7-sonnet-20250219",
-			timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-			temperature = 0,
-			max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-			--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-		},
-		behavior = {
-			enable_claude_text_editor_tool_mode = true,
-		},
-	},
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"stevearc/dressing.nvim",
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-	},
+	-- "yetone/avante.nvim",
+	-- event = "VeryLazy",
+	-- opts = {
+	-- 	-- add any opts here
+	-- 	-- for example
+	-- 	provider = "claude",
+	-- 	claude = {
+	-- 		endpoint = "https://api.anthropic.com",
+	-- 		model = "claude-3-7-sonnet-20250219",
+	-- 		timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+	-- 		temperature = 0,
+	-- 		max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+	-- 		--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+	-- 	},
+	-- 	behavior = {
+	-- 		enable_claude_text_editor_tool_mode = true,
+	-- 	},
+	-- },
+	-- dependencies = {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	"stevearc/dressing.nvim",
+	-- 	"nvim-lua/plenary.nvim",
+	-- 	"MunifTanjim/nui.nvim",
+	-- },
 }
